ffe1647abaf6ff81f301700d934c70f4c2ab47e8
Matthew Creek <matthew.creek27@gmail.com>
2024-05-15 09:41:45 -0500

    temp example

--- ReadonlyExample/Temperature/Program.cs
+++ ReadonlyExample/Temperature/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TemperatureExample;
+
+Console.WriteLine("Absolute zero in F is: " + Temperature.absoluteZeroFahrenheit);
+
+Temperature temp = new();
+
+Console.WriteLine(temp.absoluteZeroFehrenheit);
